e5d062089570f2d7a80c0272bc7f0c374c348eee
adamggrover <93373074+adamggrover@users.noreply.github.com>
2023-05-01 16:35:16 +0100

    refactor code so classes are in seperate files

--- expenses-program-v4/Program.cs
+++ expenses-program-v4/Program.cs
@@ -1,128 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class ExpenseClaim
-{
-    // Fields to store the expense cost, travel cost, non-refundable amount, and claim type
-    private double expenseCost;
-    private double travelCost;
-    private double nonRefundable;
-    private string claimType;
-
-
-    // Setter method for the expense cost field    
-    public void SetExpenseCost(double expenseCost)
-    {
-        this.expenseCost = expenseCost;
-    }       
-
-    // Getter method for the expense cost field
-    public double GetExpenseCost()
-    {
-        return expenseCost;
-    }
-
-    // Setter method for the travel cost field
-    public void SetTravelCost(double travelCost)
-    {
-        this.travelCost = travelCost;
-    }
-
-    // Getter method for the travel cost field
-    public double GetTravelCost()
-    {
-        return travelCost;
-    }
-
-    // Setter method for the non-refundable amount field
-    
-    public void SetNonRefundable(double expenseCost)
-    {
-        if (expenseCost > 50)
-        {
-            this.nonRefundable = expenseCost - 50;
-
-        }
-        else 
-        {
-            this.nonRefundable = 0;
-        }    
-            
-    }
-    
-
-    // Getter method for the non-refundable amount field
-    public double GetNonRefundable()
-    {
-        return nonRefundable;
-    }
-
-
-    // Setter method for the claim type field
-    public void SetClaimType(string claimType)
-    {
-        this.claimType = claimType;
-    }
-
-    // Getter method for the claim type field
-    public string GetClaimType()
-    {
-        return claimType;
-    }
-
-    // Method to calculate the total cost of the expense claim
-    public double GetTotalCost()
-    {
-        // Calculate the total cost by adding the expense cost and travel cost and then subtracting the non-refundable amount
-        double totalCost = expenseCost + travelCost;
-
-        return totalCost;
-    }
-
-    // Method to generate the receipt details as a list of strings
-    public List<string> GetReceiptDetails()
-    {
-        // Create a new list to store the receipt details
-        List<string> details = new List<string>();
-
-        // Add the claim type, expense cost, travel cost, non-refundable amount, and total cost to the list of receipt details
-        details.Add("Claim type: " + claimType);
-        details.Add("\nExpense cost: " + expenseCost);
-        details.Add("Travel cost: " + travelCost);
-        details.Add("Total cost: " + GetTotalCost());
-        details.Add("Non-refundable amount: " + nonRefundable);
-        
-
-        // Return the list of receipt details
-        return details;
-    }
-}
-
-
-public class Company
-{
-    // Static method to calculate the amount of tax that can be reclaimed by the company
-    // calculated at 20% of the total cost
-    public static double GetReclaimableTax(double totalCost)
-    {
-        // Multiply the total cost by 0.2 (20%) to get the amount of tax that can be reclaimed
-        return totalCost * 0.2;
-    }
-}
-
-
 
 class Program
 {
     static void Main(string[] args)
     {
 
-
-
         // Create a list of the ExpenseClaim objects
         List<ExpenseClaim> Expenseclaims = new List<ExpenseClaim>();
-
-
 
 
         // Create an array for the claim type choices
